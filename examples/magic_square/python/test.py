2f750bbca4b4969a6c3c37ea7a88d606ad0c5bb3
Henrik Finsberg <henriknf@simula.no>
2018-09-23 21:04:00 +0200

    Add dockerfile for exmaple

--- examples/magic_square/python/test.py
+++ examples/magic_square/python/test.py
@@ -1,8 +1,17 @@
-"""
-Remember to first source venv/bin/activate if the parent directory.
-"""
+import sys
 import magic
-eng = magic.initialize()
-square = eng.makesqr(5)
-print("\n".join([str(s) for s in square]))
-eng.terminate()
+
+def main(n=5):
+    
+    eng = magic.initialize()
+    square = eng.makesqr(n)
+    print("\n".join([str(s) for s in square]))
+    eng.terminate()
+
+if __name__ == "__main__":
+
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        main(int(sys.argv[1]))
+    else:
+        main()
